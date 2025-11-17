9ce85f6ba957fbdbdb637f950d95b64418909ee1
edu <edu@192.168.40.18>
2025-11-17 10:38:54 +0900

    開発開始

---
+++ spring_crud_exc5_tsutsui_ren/src/main/java/jp/co/sss/crud/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package jp.co.sss.crud.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.co.sss.crud.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	 Employee findByEmpIdAndEmpPass(Integer empId, String empPass);
+
+	 List<Employee> findAllByOrderByEmpIdAsc();
+}
